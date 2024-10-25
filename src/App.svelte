a3f42818776c70012810260ebb927f331ee7dff3
Abdul@Redmi13 <bigzero55@outlook.com>
2024-10-25 13:47:38 +0700

    tambah taileindcss dan daisy

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
   import WhatsAppInput from "./components/WhatsappTextEditor2.svelte";
   let messageText = ""; // This will be bound to the input value
+
+
 </script>
 
 <!-- Basic usage dengan two-way binding -->
-<WhatsAppInput bind:value={messageText} />
-
+<div class=" container mx-auto">
+  <WhatsAppInput bind:value={messageText} />
+</div>
 <!-- Tampilkan preview dari nilai yang di-bind -->
-<p>Output String: {messageText}</p>
+<p class="font-bold text-2xl">
+  Output String: {messageText}</p>
